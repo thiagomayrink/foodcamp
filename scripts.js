005cfa2017fcab3807f05ed9ef4c55ba6c17d149
Thiago Paiva <thiagoscmpaiva@gmail.com>
2021-04-05 14:10:27 -0300

    ajuste tipos de fonte + abrir pedido whatsapp na mesma janela + detalhes de digitacao no codigo

--- scripts.js
+++ scripts.js
@@ -98,6 +98,5 @@ function confirmar_pedido() {
     "\n Total: R$ "+valor_total.toFixed(2)+
     "\n Nome: "+nome_cliente+
     "\n Endereço: "+endereco_cliente);
-    const mensagem_wpp = 'https://wa.me/5500000000000?text='+mensagem;
-    window.open(mensagem_wpp);
+    window.open('https://wa.me/5500000000000?text='+mensagem, "_self");
 }
